f78e978c62bf3037f7995f92b3eb5d1696301060
Joojo <kojoarmahnathaniel@gmail.com>
2022-01-04 21:57:31 +0000

    updates

--- python code/First codes/While loop/While loop/While_loop.py
+++ python code/First codes/While loop/While loop/While_loop.py
@@ -5,3 +5,11 @@ while counter < 4:
     turtle.forward(100)
     turtle.right(90)
     counter = counter + 1
+
+#
+import turtle
+counter = int(1)
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter = counter + 1
