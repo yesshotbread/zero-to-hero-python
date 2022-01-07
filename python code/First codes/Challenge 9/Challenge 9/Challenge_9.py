c4bbd0ecbc2793490606c9ddaf9d8d631269f83d
Joojo <kojoarmahnathaniel@gmail.com>
2022-01-07 16:19:31 +0000

    update

---
+++ python code/First codes/Challenge 9/Challenge 9/Challenge_9.py
@@ -0,0 +1,10 @@
+guests = []
+name = " "
+
+name = input("Enter guest name (Enter 'DONE' if complete): ")
+while name in guests != "DONE":
+    name = input("Enter guest name (Enter 'DONE' if complete): ")
+    guests.append(name)
+
+for name in guests:
+    print (guests)
