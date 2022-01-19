a93b9abb6d058da0f7f9c4a0c9eec9117ba1c2cb
Joojo <kojoarmahnathaniel@gmail.com>
2022-01-19 21:13:31 +0000

    update

--- python code/First codes/Challenge 9/Challenge 9/Challenge_9.py
+++ python code/First codes/Challenge 9/Challenge 9/Challenge_9.py
@@ -4,6 +4,7 @@ guest = []
 
 while name != "DONE":
     name = input("Enter the name of the guest (Enter 'DONE' when list ends): ")
+    name = name.upper()
     guest.append(name)
 
     guest.sort()
---
+++ python code/First codes/Functions/Functions/Functions.py
@@ -0,0 +1,54 @@
+## Defining and calling a function
+
+#def printMessage():
+#    print("Hello, World!")
+#    return
+#printMessage()
+
+## Using the main function
+
+#def main():
+#    printMessage()
+#    return
+
+#def printMessage():
+#    print("Hello, World!")
+#    return
+
+#main()
+
+## Using in loops
+
+#def main():
+#    printName()
+#    return
+
+#def printName():
+#    names = ["Linda", "Barbara", "Monica", "Jesica", "Pamela", "Sarah", "Gifty", "Diana"]
+#    for name in names:
+#        print(name)
+#    return
+
+#main()
+
+## Multiple lines
+
+#def displaymessage(greeting, name):
+#    message = (greeting +", "+ name)
+#    print(message)
+#    return
+
+#displaymessage("Hi", "Hotbread")
+
+##
+
+#def printnames():
+#    names = ["Linda", "Barbara", "Monica", "Jessica"]
+#    newname = input("Enter last name? ")
+#    names.append(newname)
+
+#    print(names)
+
+#printnames()
+
+## Returning data
